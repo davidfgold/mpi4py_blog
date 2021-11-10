3554dd3832ad260974484b900f986b682467ef7e
davidfgold <dgoldri25@gmail.com>
2021-11-10 12:07:07 -0500

    Adding better comments

--- callDemoScript.py
+++ callDemoScript.py
@@ -1,3 +1,12 @@
+'''
+callDemoScript.py
+
+Called to batch demoScript.m across multiple MPI tasks
+
+Reads in the total tasks and number of samples per task from command line.
+'''
+
+# load necessary libraries
 from mpi4py import MPI
 import numpy as np
 import sys
@@ -12,17 +21,19 @@ rank = comm.Get_rank()
 
 # read in arguments from the submission script
 TOTAL_TASKS = int(sys.argv[1]) # number of MPI processes
-N_RUNS_PER_TASK = int(sys.argv[2]) # number of runs per/task
+N_SAMPLES_PER_TASK = int(sys.argv[2]) # number of runs per/task
 
 # loop through runs assigned to current rank
-for i in range(N_RUNS_PER_TASK):
-	RUN = rank + TOTAL_TASKS * i
+for i in range(N_SAMPLES_PER_TASK):
+	sample= rank + TOTAL_TASKS * i
 
 	# write the command that will be sent to the terminal (here RUN will replace the {})
-	terminal_command = "octave-cli ./demoScript.m {} {} ".format(rank, RUN)
+	terminal_command = "octave-cli ./demoScript.m {} {} ".format(rank, sample)
 
 	# write the terminal command to the process
 	os.system(terminal_command)
 
 	# sleep before submitting the next command
 	time.sleep(1) # optional, for memory intensive submissions
+
+comm.Barrier()
